a33587ec50e479ca997399bb561d8f5aa336449d
Clément COUILLAUD <clement@hoplie.com>
2023-07-18 17:26:29 +0200

    ajout category

---
+++ src/Entity/Admin/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity\Admin;
+
+use App\Repository\Admin\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category implements TimestampableInterface
+{
+    use TimestampableTrait;
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $wording = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWording(): ?string
+    {
+        return $this->wording;
+    }
+
+    public function setWording(string $wording): static
+    {
+        $this->wording = $wording;
+
+        return $this;
+    }
+}
---
+++ src/Form/Admin/CategoryType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Admin\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('wording')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
